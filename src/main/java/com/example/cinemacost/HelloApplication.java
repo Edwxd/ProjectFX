bf3ef6893443c69b39c793190d9dc94e6bd68c75
Edwxd <nasser.edward1@gmail.com>
2022-05-23 20:00:12 -0400

    First part of the project

--- src/main/java/com/example/cinemacost/HelloApplication.java
+++ src/main/java/com/example/cinemacost/HelloApplication.java
@@ -17,7 +17,7 @@ import java.io.IOException;
 
 public  class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) {
 
 
 
